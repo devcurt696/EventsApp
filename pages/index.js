648635ceeedcfc87b2ea15f04e4d855f5c287bf9
curtcurt69 <cecrouse96@yahoo.com>
2023-12-11 19:47:33 -0500

    Simple events app in nextjs

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
+function Home() {
+    const featuredEvents = getFeaturedEvents();
+
+    return (
+        <div>
+            <EventList items={featuredEvents} />
+        </div>
+    )
+}
+
+export default Home;
